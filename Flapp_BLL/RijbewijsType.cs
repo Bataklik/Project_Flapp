17423a4474fdffddc1fca020a76e3fa4a2f196d8
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-09-30 12:55:19 +0200

    Add RijbewijsType

---
+++ Flapp_BLL/RijbewijsType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flapp_BLL
+{
+    public enum RijbewijsType
+    {
+        A, AM, B, C, D, G
+    }
+}
