fa435a42312807f839702f4e677b3899ebda900d
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-09-30 12:55:28 +0200

    Add Tankkaart

---
+++ Flapp_BLL/Tankkaart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flapp_BLL
+{
+    public class Tankkaart
+    {
+        private int _kaartnummer;
+        private DateTime _geldigheidsdatum;
+        private int _pincode;
+        private string _brandstoftype;
+        private Bestuurder _bestuurder;
+    }
+}
