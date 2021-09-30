0fd8a94cb84426954f2a26d4f3351f455b160023
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-09-30 12:54:54 +0200

    Add Bestuurder

---
+++ Flapp_BLL/Bestuurder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flapp_BLL
+{
+    public class Bestuurder
+    {
+        private int _naam;
+        private int _voornaam;
+        private Adres _adres;
+        private int _geboortedatum;
+        private int _rijksregisternummer;
+    }
+}
