dcd594cdfa8584776a42b1f9b35d64edd2890256
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-09-30 18:32:58 +0200

    Add Rijksregisternummer

---
+++ Flapp_BLL/Utils/Rijksregisternummer.cs
@@ -0,0 +1,21 @@
+﻿using Flapp_BLL.Exceptions;
+using System.Linq;
+
+namespace Flapp_BLL.Utils
+{
+    public class Rijksregisternummer
+    {
+        private string _nummer;
+
+        public Rijksregisternummer(string r)
+        {
+            if (r.Count(e => char.IsDigit(e)) != 11) { throw new RijksregisternummerException("Het identificatienummer bevat 11 cijfers"); }
+            _nummer = r;
+        }
+
+        public string ToonNummer()
+        {
+            return _nummer;
+        }
+    }
+}
