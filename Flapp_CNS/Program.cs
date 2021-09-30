a14ee094429e2fdaaa99fefa16879d8b3cd3d507
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-09-30 18:33:39 +0200

    Add Console project

---
+++ Flapp_CNS/Program.cs
@@ -0,0 +1,13 @@
+﻿using Flapp_BLL.Utils;
+using System;
+
+namespace Flapp_CNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Rijksregisternummer("90.02.01-999-02").ToonNummer());
+        }
+    }
+}
