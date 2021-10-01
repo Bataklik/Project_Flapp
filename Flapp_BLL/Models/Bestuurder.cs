c2f4145c44a5c86c0b1fe9589c5ee9df2738ed5b
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-01 12:47:09 +0200

    Update Models

--- Flapp_BLL/Models/Bestuurder.cs
+++ Flapp_BLL/Models/Bestuurder.cs
@@ -6,14 +6,23 @@ namespace Flapp_BLL.Models
 {
     public class Bestuurder
     {
-        private string _naam;
-        private string _voornaam;
+        private string _naam; //!
+        private string _voornaam; //!
         private Adres _adres;
-        private DateTime _geboortedatum;
-        private string _rijksregisternummer;
-        private RijbewijsType _rijbewijs;
+        private DateTime _geboortedatum; //!
+        private Rijksregisternummer _rijksregisternummer; //!
+        private RijbewijsType _rijbewijs; //!
         private Voertuig _voertuig;
         private Tankkaart _tankkaart;
+
+        public Bestuurder(string naam, string voornaam, DateTime geboortedatum, Rijksregisternummer rijksregisternummer, RijbewijsType rijbewijs)
+        {
+            _naam = naam;
+            _voornaam = voornaam;
+            _geboortedatum = geboortedatum;
+            _rijksregisternummer = rijksregisternummer;
+            _rijbewijs = rijbewijs;
+        }
 
         public string Naam
         {
@@ -38,12 +47,26 @@ namespace Flapp_BLL.Models
         public Adres Adres
         {
             get => _adres;
-            set
-            {
-                _adres = value ?? throw new AdresException("Adres mag niet leeg zijn!");
-            }
         }
 
+        public DateTime Geboortedatum
+        {
+            get => _geboortedatum;
+            set => _geboortedatum = value;
+        }
 
+        public Rijksregisternummer Rijksregisternummer { get => _rijksregisternummer; }
+
+        public RijbewijsType Rijbewijs { get => _rijbewijs; set => _rijbewijs = value;; }
+
+        public Voertuig Voertuig
+        {
+            get => _voertuig;
+        }
+
+        public Tankkaart Tankkaart
+        {
+            get => _tankkaart;
+        }
     }
 }
--- Flapp_BLL/Models/Tankkaart.cs
+++ Flapp_BLL/Models/Tankkaart.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using Flapp_BLL.Exceptions;
+using System;
 
 namespace Flapp_BLL.Models
 {
     public class Tankkaart
     {
-        private int _kaartnummer;
-        private DateTime _geldigheidsdatum;
+        private int _kaartnummer; //!
+        private DateTime _geldigheidsdatum; //!
         private int _pincode;
         private string _brandstoftype;
         private Bestuurder _bestuurder;
+        private bool _geblokkeerd;
+
+
+
+        public int Kaartnummer { get => _kaartnummer; set => _kaartnummer = value; }
+        public DateTime Geldigheidsdatum
+        {
+            get => _geldigheidsdatum;
+            set
+            {
+                if (value < DateTime.Now) { throw new GeldigeheidsdatumException("Geledigheidsdatum is al verlopen!"); }
+                _geldigheidsdatum = value;
+            }
+        }
+        public int Pincode { get => _pincode; }
+        public string Brandstof { get => _brandstoftype; }
     }
 }
