9508874f9a2e1e9da21a3747b40554e5da1f12c4
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-02 18:52:49 +0200

    Update Rijkregisternummer

--- Flapp_BLL/Utils/Rijksregisternummer.cs
+++ Flapp_BLL/Utils/Rijksregisternummer.cs
@@ -1,5 +1,4 @@
 ﻿using Flapp_BLL.Exceptions;
-using Flapp_BLL.Models;
 using System;
 using System.Linq;
 
@@ -8,65 +7,73 @@ namespace Flapp_BLL.Utils
     public class Rijksregisternummer
     {
         private string _nummer;
-        private string _datum;
+        //private string _datum;
 
         public Rijksregisternummer(string r)
         {
-            //Rijksregisternummer
-            if (r.Count(e => char.IsDigit(e)) != 11) { throw new RijksregisternummerException("Het identificatienummer bevat 11 cijfers"); }
-            
-            _nummer = r;
-            
+            // 90.02.01-999.02
+            if (ControleRijksgisternummer(r))
+                _nummer = r;
         }
-
-        public Rijksregisternummer(DateTime dt) {
-            if (rijksregisterControle(dt)) { throw new RijksregisternummerException("De geboortedatum komt niet overeen met het rijksregisternummer"); }
-            _datum = datumToString(dt);
-        }
-
-        public string datumToString(DateTime geboorteDatum) {
-            _datum = geboorteDatum.ToString("dd-MM-yy");
-            return _datum;
-        }
-
-        public bool rijksregisterControle(DateTime geboorteDatum) {
-            _datum = datumToString(geboorteDatum);
-
-            string dagGeboorte = _datum[0].ToString();
-            dagGeboorte += _datum[1].ToString();
-
-            string maandGeboorte = _datum[3].ToString();
-            maandGeboorte += _datum[4].ToString();
-
-            string jaarGeboorte = _datum[6].ToString();
-            jaarGeboorte += _datum[7].ToString();
-
-            string jaarRijksregister = _nummer[0].ToString();
-            jaarRijksregister += _nummer[1];
-
-            string maandRijksregister = _nummer[3].ToString();
-            maandRijksregister += _nummer[4];
-
-            string dagRijksregister = _nummer[6].ToString();
-            dagRijksregister += _nummer[7];
-
-            if (dagGeboorte == dagRijksregister && maandGeboorte == maandRijksregister && jaarGeboorte == jaarRijksregister)
-               return true;
-            else 
-               return false;
-        }
-
         public string ToonNummer()
         {
             return _nummer;
         }
 
-        public Rijksregisternummer VeranderNummer(string r)
+        public void VeranderNummer(string r)
+        {
+            if (ControleRijksgisternummer(r))
+                _nummer = r;
+        }
+        private bool ControleRijksgisternummer(string r)
         {
             if (r.Count(e => char.IsDigit(e)) != 11) { throw new RijksregisternummerException("Het identificatienummer bevat 11 cijfers"); }
-            _nummer = r;
-            return this;
+            if (r.Count(e => e == '.') != 3) { throw new RijksregisternummerException("Het Rijksregisternummer is ongeldig!"); }
+            return true;
         }
+        #region Code
+        //public Rijksregisternummer(DateTime dt)
+        //{
+        //    /* Rijksregisternummer kun je niet zomaar uitrekenen,
+        //       want groep twee heb je drie cijfers nodig die dienen om personen te herkennen die op dezelfde dag geboren zijn.
+        //       bv: Tibo (01,01,1999) dan krijg je 001, Raf ook zelfde dag geboren krijgt hij 003 (mannen krijgen oneven cijfers/getallen)
+        //     */
+        //    if (rijksregisterControle(dt)) { throw new RijksregisternummerException("De geboortedatum komt niet overeen met het rijksregisternummer"); }
+        //    _datum = datumToString(dt);
+        //}
+        //public string datumToString(DateTime geboorteDatum)
+        //{
+        //    _datum = geboorteDatum.ToString("dd-MM-yy");
+        //    return _datum;
+        //}
+        //public bool rijksregisterControle(DateTime geboorteDatum)
+        //{
+        //    _datum = datumToString(geboorteDatum);
+
+        //    string dagGeboorte = _datum[0].ToString();
+        //    dagGeboorte += _datum[1].ToString();
+
+        //    string maandGeboorte = _datum[3].ToString();
+        //    maandGeboorte += _datum[4].ToString();
+
+        //    string jaarGeboorte = _datum[6].ToString();
+        //    jaarGeboorte += _datum[7].ToString();
+
+        //    string jaarRijksregister = _nummer[0].ToString();
+        //    jaarRijksregister += _nummer[1];
+
+        //    string maandRijksregister = _nummer[3].ToString();
+        //    maandRijksregister += _nummer[4];
+
+        //    string dagRijksregister = _nummer[6].ToString();
+        //    dagRijksregister += _nummer[7];
+
+        //    if (dagGeboorte == dagRijksregister && maandGeboorte == maandRijksregister && jaarGeboorte == jaarRijksregister)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        #endregion
 
         #region Override
         public override bool Equals(object obj)
