458e8935776bcb89898d2a5676158df7a9e92f70
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-08 11:01:30 +0200

    Refactor Bestuurder

--- Flapp_BLL/Models/Bestuurder.cs
+++ Flapp_BLL/Models/Bestuurder.cs
@@ -7,96 +7,79 @@ namespace Flapp_BLL.Models
 {
     public class Bestuurder
     {
-        private string _naam; //!
-        private string _voornaam; //!
-        private Adres _adres;
-        private DateTime _geboortedatum; //!
-        private Rijksregisternummer _rijksregisternummer; //!
-        private RijbewijsType _rijbewijs; //!
-        private Voertuig _voertuig;
-        private Tankkaart _tankkaart;
 
         #region Constructors
         public Bestuurder(string naam, string voornaam, DateTime geboortedatum, Rijksregisternummer rijksregisternummer, RijbewijsType rijbewijs)
         {
-            Naam = naam;
-            Voornaam = voornaam;
-            Geboortedatum = geboortedatum;
-            Rijksregisternummer = rijksregisternummer;
-            Rijbewijs = rijbewijs;
+            ZetNaam(naam);
+            ZetVoornaam(voornaam);
+            ZetGeboortedatum(geboortedatum);
+            ZetRijksregisternummer(rijksregisternummer);
+            ZetRijbijsType(rijbewijs);
         }
 
-        public Bestuurder(string naam, string voornaam, Adres adres, DateTime geboortedatum, Rijksregisternummer rijksregisternummer, RijbewijsType rijbewijs, Voertuig voertuig, Tankkaart tankkaart){
-            Naam = naam;
-            Voornaam = voornaam;
-            Geboortedatum = geboortedatum;
-            Rijksregisternummer = rijksregisternummer;
-            Rijbewijs = rijbewijs;
-            Adres = adres;
-            Voertuig = voertuig;
-            Tankkaart = tankkaart;
+        public Bestuurder(string naam, string voornaam, Adres adres, DateTime geboortedatum, Rijksregisternummer rijksregisternummer, RijbewijsType rijbewijs, Voertuig voertuig, Tankkaart tankkaart)
+        {
+            ZetNaam(naam);
+            ZetVoornaam(voornaam);
+            ZetGeboortedatum(geboortedatum);
+            ZetRijksregisternummer(rijksregisternummer);
+            ZetRijbijsType(rijbewijs);
+            ZetAdres(adres);
+            ZetVoertuig(voertuig);
+            ZetTankkaart(tankkaart);
+        }
+        #endregion
+
+        #region ZetMethods
+        public void ZetNaam(string n)
+        {
+            if (string.IsNullOrWhiteSpace(n)) { throw new BestuurderException("Naam mag niet leeg zijn!"); }
+            Naam = n;
+        }
+        public void ZetVoornaam(string n)
+        {
+            if (string.IsNullOrWhiteSpace(n)) { throw new BestuurderException("Naam mag niet leeg zijn!"); }
+            Voornaam = n;
+        }
+        public void ZetAdres(Adres a)
+        {
+            if (a == null) { throw new BestuurderException("Bestuurder adres is null!"); }
+            Adres = a;
+        }
+        public void ZetGeboortedatum(DateTime d)
+        {
+            Geboortedatum = d;
+        }
+        public void ZetRijksregisternummer(Rijksregisternummer r)
+        {
+            if (r == null) { throw new BestuurderException("Bestuuder rijksregisternummer is null!"); }
+            Rijksregisternummer = r;
+        }
+        public void ZetRijbijsType(RijbewijsType rt)
+        {
+            RijbewijsType = rt;
+        }
+        public void ZetVoertuig(Voertuig v)
+        {
+            if (v == null) { throw new BestuurderException("Bestuuder voertuig is null!"); }
+            Voertuig = v;
+        }
+        public void ZetTankkaart(Tankkaart tk)
+        {
+            Tankkaart = tk ?? throw new BestuurderException("Bestuurder tankkaart is null!");
         }
         #endregion
 
         #region Props
-        public string Naam
-        {
-            get => _naam;
-            set
-            {
-                if (string.IsNullOrEmpty(value)) { throw new LeegException("Naam mag niet leeg zijn!"); }
-                _naam = value;
-            }
-        }
-
-        public string Voornaam
-        {
-            get => _voornaam;
-            set
-            {
-                if (string.IsNullOrEmpty(value)) { throw new LeegException("Voornaam mag niet leeg zijn!"); }
-                _voornaam = value;
-            }
-        }
-
-        public Adres Adres
-        {
-            get => _adres;
-            set => _adres = value;
-        }
-
-        public DateTime Geboortedatum
-        {
-            get => _geboortedatum;
-            set
-            {
-                _geboortedatum = value.ToUniversalTime();
-            }
-        }
-
-        public Rijksregisternummer Rijksregisternummer
-        {
-            get => _rijksregisternummer;
-            private set => _rijksregisternummer = value;
-        }
-
-        public RijbewijsType Rijbewijs
-        {
-            get => _rijbewijs;
-            set => _rijbewijs = value;
-        }
-
-        public Voertuig Voertuig
-        {
-            get => _voertuig;
-            private set => _voertuig = value;
-        }
-
-        public Tankkaart Tankkaart
-        {
-            get => _tankkaart;
-            private set => _tankkaart = value;
-        }
+        public string Naam { get; private set; }
+        public string Voornaam { get; private set; }
+        public Adres Adres { get; private set; }
+        public DateTime Geboortedatum { get; private set; }
+        public Rijksregisternummer Rijksregisternummer { get; private set; }
+        public RijbewijsType RijbewijsType { get; private set; }
+        public Voertuig Voertuig { get; private set; }
+        public Tankkaart Tankkaart { get; private set; }
         #endregion
 
         #region Methods
@@ -107,23 +90,19 @@ namespace Flapp_BLL.Models
         public override bool Equals(object obj)
         {
             return obj is Bestuurder bestuurder &&
-                   _naam == bestuurder._naam &&
-                   _voornaam == bestuurder._voornaam &&
-                   EqualityComparer<Adres>.Default.Equals(_adres, bestuurder._adres) &&
-                   _geboortedatum == bestuurder._geboortedatum &&
-                   EqualityComparer<Rijksregisternummer>.Default.Equals(_rijksregisternummer, bestuurder._rijksregisternummer) &&
-                   _rijbewijs == bestuurder._rijbewijs &&
-                   EqualityComparer<Voertuig>.Default.Equals(_voertuig, bestuurder._voertuig) &&
-                   EqualityComparer<Tankkaart>.Default.Equals(_tankkaart, bestuurder._tankkaart);
+                   Naam == bestuurder.Naam &&
+                   Voornaam == bestuurder.Voornaam &&
+                   EqualityComparer<Adres>.Default.Equals(Adres, bestuurder.Adres) &&
+                   Geboortedatum == bestuurder.Geboortedatum &&
+                   EqualityComparer<Rijksregisternummer>.Default.Equals(Rijksregisternummer, bestuurder.Rijksregisternummer) &&
+                   RijbewijsType == bestuurder.RijbewijsType &&
+                   EqualityComparer<Voertuig>.Default.Equals(Voertuig, bestuurder.Voertuig) &&
+                   EqualityComparer<Tankkaart>.Default.Equals(Tankkaart, bestuurder.Tankkaart);
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(_naam, _voornaam, _adres, _geboortedatum, _rijksregisternummer, _rijbewijs, _voertuig, _tankkaart);
-        }
-
-        public override string ToString() {
-            return $"[Bestuurder] {_naam}, {_voornaam}, {_adres}, {_geboortedatum}, {_rijksregisternummer}, {_rijbewijs}, {_voertuig}, {_tankkaart}";
+            return HashCode.Combine(Naam, Voornaam, Adres, Geboortedatum, Rijksregisternummer, RijbewijsType, Voertuig, Tankkaart);
         }
         #endregion
     }
