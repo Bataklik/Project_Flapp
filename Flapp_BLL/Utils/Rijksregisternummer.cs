54392d1aa47609d1bcd1461f483ce4fb3eb2042a
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-08 11:02:04 +0200

    Add false to ControleEersteGroep

--- Flapp_BLL/Utils/Rijksregisternummer.cs
+++ Flapp_BLL/Utils/Rijksregisternummer.cs
@@ -5,34 +5,41 @@ using Flapp_BLL.Models;
 
 namespace Flapp_BLL.Utils
 {
-    public class Rijksregisternummer {
+    public class Rijksregisternummer
+    {
         private string _nummer;
         public Bestuurder _bestuurder;
 
-        public Rijksregisternummer(string r) {
+        public Rijksregisternummer(string r)
+        {
             if (ControleRijksgisternummer(r))
                 _nummer = r;
         }
-        public string ToonNummer() {
+        public string ToonNummer()
+        {
             return _nummer;
         }
-        public void VeranderNummer(string r) {
+        public void VeranderNummer(string r)
+        {
             if (ControleRijksgisternummer(r))
                 _nummer = r;
         }
-        private bool ControleRijksgisternummer(string r) {
+        private bool ControleRijksgisternummer(string r)
+        {
             if (r.Count(e => char.IsDigit(e)) != 11) { throw new RijksregisternummerException("Het identificatienummer bevat 11 cijfers"); }
             if (r.Count(e => e == '.') != 3) { throw new RijksregisternummerException("Het Rijksregisternummer is ongeldig!"); }
             if (r.Count(e => e == '-') != 1) { throw new RijksregisternummerException("Het Rijksregisternummer is ongeldig!"); }
             return true;
         }
-        public void ZetBestuurder(Bestuurder newBestuurder) {
+        public void ZetBestuurder(Bestuurder newBestuurder)
+        {
             if (newBestuurder == null) throw new Exception();
             if (newBestuurder == _bestuurder) throw new Exception();
             if (_bestuurder != null)
                 _bestuurder = newBestuurder;
         }
-        public bool ControleEersteGroep(string r) {
+        public bool ControleEersteGroep(string r)
+        {
             DateTime datetime = _bestuurder.Geboortedatum;
             string datum = datetime.ToString("dd/MM/y");
 
@@ -50,6 +57,8 @@ namespace Flapp_BLL.Utils
             //21.10.02-289.65
             string rijksJaar = _nummer[0].ToString();
             rijksJaar += _nummer[1].ToString();
+
+            return false;
         }
 
 
@@ -65,7 +74,8 @@ namespace Flapp_BLL.Utils
             return HashCode.Combine(_nummer);
         }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return $"[Rijksregisternummer] {_nummer}";
         }
         #endregion
