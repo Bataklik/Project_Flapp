f3669c53dbc621d405407b10fc6ae74e97fec6a7
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-10 19:32:59 +0200

    Add ctor Test + ZetBrandstofNaam Tests

--- Flapp_Tests/Brandstof_UnitTest.cs
+++ Flapp_Tests/Brandstof_UnitTest.cs
@@ -5,15 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Flapp_BLL.Models;
+using Flapp_BLL.Exceptions;
 
-namespace Flapp_TESTS {
-    public class Brandstof_UnitTest {
+namespace Flapp_TESTS
+{
+    public class Brandstof_UnitTest
+    {
         #region Ctor Tests
         [Fact]
-        public void Test_ctor1_Valid() {
+        public void Test_ctor_Valid()
+        {
             Brandstof b = new Brandstof("Benzine");
 
             Assert.Equal("Benzine", b.Naam);
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("    ")]
+        public void Test_ctor_BadNaam_InValid(string naam)
+        {
+            Assert.Throws<BrandstofException>(() => new Brandstof(naam));
+        }
+        #endregion
+
+        #region ZetBrandstofNaam Tests
+        [Fact]
+        public void Test_ZetBrandstofNaam_Valid()
+        {
+            Brandstof b = new Brandstof("enzine");
+            b.ZetBrandstofNaam("Benzine");
+            Assert.Equal("Benzine", b.Naam);
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("    ")]
+        public void Test_ZetBrandstofNaam_BadNaam_InValid(string naam)
+        {
+            Brandstof b = new Brandstof("enzine");
+            Assert.Throws<BrandstofException>(() => b.ZetBrandstofNaam(naam));
         }
         #endregion
     }
