84d34bac52b60f6138c12b46f6aa038ee624fa87
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-10 11:23:37 +0200

    Refactor Adres

--- Flapp_BLL/Utils/Adres.cs
+++ Flapp_BLL/Utils/Adres.cs
@@ -8,67 +8,63 @@ namespace Flapp_BLL.Utils
 {
     public class Adres
     {
-        private string _straat;
-        private int _huisnummer;
-        private string _stad;
-        private int _postcode;
+        #region Props
+        public string Straat { get; private set; }
+        public int Huisnummer { get; private set; }
+        public string Stad { get; private set; }
+        public int Postcode { get; private set; }
+        #endregion
+
+        #region ZetMethods
+        public void ZetStraat(string value)
+        {
+            if (string.IsNullOrEmpty(value)) { throw new StraatException("Straatnaam mag niet leeg zijn!"); }
+            Straat = value;
+        }
+        public void ZetHuisnummer(int value)
+        {
+            Huisnummer = value;
+        }
+        public void ZetStad(string value)
+        {
+            if (string.IsNullOrEmpty(value)) { throw new StadException("Stadnaam mag niet leeg zijn!"); }
+            Stad = value;
+        }
+        public void ZetPostcode(int value)
+        {
+            if (value < 1000 || value > 9999) { throw new PostcodeException("Postcodes zijn groter dan 1000 en kleiner dan 9999"); }
+            Postcode = value;
+        }
+        #endregion
 
         #region Construtors
         public Adres(string straat, int huisnummer, string stad, int postcode)
         {
-            Straat = straat;
-            Huisnummer = huisnummer;
-            Stad = stad;
-            Postcode = postcode;
-        }
-        #endregion
-
-        #region Props
-        public string Straat {
-            get => _straat;
-            private set {
-                if (string.IsNullOrEmpty(value)) { throw new StraatException("Straatnaam mag niet leeg zijn!"); }
-                _straat = value;
-            }
-        }
-        public int Huisnummer {
-            get => _huisnummer;
-            private set {
-                _huisnummer = value;
-            }
-        }
-        public string Stad {
-            get => _stad;
-            private set {
-                if (string.IsNullOrEmpty(value)) { throw new StadException("Stadnaam mag niet leeg zijn!"); }
-                _stad = value;
-            }
-        }
-        public int Postcode {
-            get => _postcode; private set {
-                if (value < 1000 || value > 9999) { throw new PostcodeException("Postcodes zijn groter dan 1000 en kleiner dan 9999"); }
-                _postcode = value;
-            }
+            ZetStraat(straat);
+            ZetHuisnummer(huisnummer);
+            ZetStad(stad);
+            ZetPostcode(postcode);
         }
         #endregion
 
         #region Overrides
+        public override string ToString()
+        {
+            return $"[Adres] {Straat}, {Huisnummer}, {Stad}: {Postcode}";
+        }
+
         public override bool Equals(object obj)
         {
             return obj is Adres adres &&
-                   _straat == adres._straat &&
-                   _huisnummer == adres._huisnummer &&
-                   _stad == adres._stad &&
-                   _postcode == adres._postcode;
+                   Straat == adres.Straat &&
+                   Huisnummer == adres.Huisnummer &&
+                   Stad == adres.Stad &&
+                   Postcode == adres.Postcode;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(_straat, _huisnummer, _stad, _postcode);
-        }
-
-        public override string ToString() {
-            return $"[Adres] {Straat}, {Huisnummer}, {Stad}, {Postcode}";
+            return HashCode.Combine(Straat, Huisnummer, Stad, Postcode);
         }
         #endregion
     }
