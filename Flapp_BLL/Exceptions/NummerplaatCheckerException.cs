495c0630d0d34491ea7ef38ebeeae65fccc4509b
Tibo <Tibo@DESKTOP-8JVOTB1>
2021-10-11 13:52:06 +0200

    Update nummerplaatchecker

---
+++ Flapp_BLL/Exceptions/NummerplaatCheckerException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Flapp_BLL.Exceptions
+{
+    internal class NummerplaatCheckerException : Exception
+    {
+        public NummerplaatCheckerException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Flapp_BLL/Utils/NummerplaatChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Flapp_BLL.Models;
+
+namespace Flapp_BLL.Utils {
+    public class NummerplaatChecker {
+
+        #region Props
+        public string Nummerplaat { get; private set; }
+        #endregion
+
+        #region Constructors
+        public NummerplaatChecker(string n) {
+            this.Nummerplaat = n;
+            Bestuurder b = new Bestuurder();
+        }
+        #endregion
+
+        #region Methods
+        public bool ControleNummerplaat(string n) {
+            //1-ABC-123
+            if (n.Count(e => char.IsDigit(e)) != 9) { throw new NummerplaatCheckerException("Een nummerplaat bevat 9 cijfers"); }
+            if (r.Count(e => e == '-') != 2) { throw new NummerplaatCheckerException("Het nummerplaat is ongeldig!"); }
+        }
+        #endregion
+    }
+}
+
