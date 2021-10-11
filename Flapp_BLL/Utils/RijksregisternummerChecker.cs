8a6108f33dc7df88e7845bb9860b4f22425f8e89
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-11 18:26:36 +0200

    Update Geslacht string -> ENUM

--- Flapp_BLL/Utils/RijksregisternummerChecker.cs
+++ Flapp_BLL/Utils/RijksregisternummerChecker.cs
@@ -11,23 +11,25 @@ namespace Flapp_BLL.Utils
         #region Props
         public string Nummer { get; private set; }
         public DateTime Geboortedatum { get; private set; }
-        public string Geslacht { get; private set; }
+        public Geslacht Geslacht { get; private set; }
         #endregion
 
         #region Constructors
-        public RijksregisternummerChecker(string r, DateTime gd, string g)
+        public RijksregisternummerChecker(string r, DateTime gd, Geslacht g)
         {
-            if (ControleRijksgisternummer(r, gd, g)) {
-                this.Nummer = r;
-                this.Geboortedatum = gd;
-                this.Geslacht = g;
+            if (ControleRijksgisternummer(r, gd, g))
+            {
+                Nummer = r;
+                Geboortedatum = gd;
+                Geslacht = g;
             }
-            
+
         }
         #endregion
 
         #region Methods
-        public bool ControleRijksgisternummer(string r, DateTime gd, string g) {
+        public bool ControleRijksgisternummer(string r, DateTime gd, Geslacht g)
+        {
             if (r.Count(e => char.IsDigit(e)) != 11) { throw new RijksregisternummerCheckerException("Het identificatienummer bevat 11 cijfers"); }
             if (r.Count(e => e == '.') != 3) { throw new RijksregisternummerCheckerException("Het Rijksregisternummer is ongeldig!"); }
             if (r.Count(e => e == '-') != 1) { throw new RijksregisternummerCheckerException("Het Rijksregisternummer is ongeldig!"); }
@@ -36,52 +38,45 @@ namespace Flapp_BLL.Utils
             return true;
         }
 
-        private bool ControleEersteGroep(string r, DateTime gd) {
+        private bool ControleEersteGroep(string r, DateTime gd)
+        {
             DateTime datetime = gd;
             string datum = datetime.ToString("dd/MM/y");
-            
+
             string rijksnr = r;
 
             //dd/MM/jj
             string maandDatum = datum[0].ToString() + datum[1].ToString();
-            
+
             string dagDatum = datum[3].ToString() + datum[4].ToString();
-   
+
             string jaarDatum = datum[6].ToString() + datum[7].ToString();
-       
+
             //21.10.02-289.65
             string rijksJaar = rijksnr[0].ToString() + rijksnr[1].ToString();
-      
+
             string rijksMaand = rijksnr[3].ToString() + rijksnr[4].ToString();
-       
+
             string rijksDag = rijksnr[6].ToString() + rijksnr[7].ToString();
-        
-            if (rijksJaar == jaarDatum && rijksDag == dagDatum && rijksMaand == maandDatum) {
+
+            if (rijksJaar == jaarDatum && rijksDag == dagDatum && rijksMaand == maandDatum)
+            {
                 return true;
             }
-            else {
-                return false;
-            }
+            else { return false; }
         }
 
-        private bool ControleTweedeGroep(string r, string g) {
+        private bool ControleTweedeGroep(string r, Geslacht g)
+        {
             //21.10.02-289.65
             string tweedeGroep = r[9].ToString() + r[10].ToString() + r[11].ToString();
-            g = g.ToUpper();
             //Man van 001 tot 997
             //Vrouw van 002 tot 998.
             int intTweedeGroep = Convert.ToInt32(tweedeGroep);
-            
-            if (g == "M" && intTweedeGroep % 2 != 0) {
-                return true;
-            }
-            else if (g == "V" && intTweedeGroep % 2 == 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
 
+            if (g == Geslacht.M && intTweedeGroep % 2 != 0) { return true; }
+            else if (g == Geslacht.V && intTweedeGroep % 2 == 0) { return true; }
+            else { return false; }
         }
         #endregion
 
