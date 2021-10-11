5341aeaa13ae09417cf74b79e6d0b0cf0be7b551
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-11 18:28:24 +0200

    Not Finished

--- Flapp_Tests/Bestuurder_UnitTest.cs
+++ Flapp_Tests/Bestuurder_UnitTest.cs
@@ -1,3 +1,4 @@
+using Flapp_BLL.Exceptions;
 using Flapp_BLL.Models;
 using Flapp_BLL.Utils;
 using System;
@@ -7,19 +8,38 @@ namespace Flapp_TESTS
 {
     public class Bestuurder_UnitTest
     {
+        // Ctor 1
         #region Ctor Tests
         [Fact]
         public void Test_ctor1_Valid()
         {
-            Bestuurder b = new("Balci", "Burak", "M", DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B);
+            Bestuurder b = new("Balci", "Burak", Geslacht.M, DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B);
 
             Assert.Equal("Balci", b.Naam);
             Assert.Equal("Burak", b.Voornaam);
-            Assert.Equal("M", b.Geslacht);
+            Assert.Equal(Geslacht.M, b.Geslacht);
             Assert.Equal(DateTime.Parse("12/05/1999"), b.Geboortedatum);
             Assert.Equal("99.05.12-273.26", b.Rijksregisternummer);
             Assert.Equal(RijbewijsType.B, b.RijbewijsType);
         }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Test_ctor1_BadNaam_InValid(string naam)
+        {
+            Assert.Throws<BestuurderException>(() => new Bestuurder(naam, "Burak", Geslacht.M, DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B));
+        }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Test_ctor1_BadVoorNaam_InValid(string voornaam)
+        {
+            Assert.Throws<BestuurderException>(() => new Bestuurder("Burak", voornaam, Geslacht.M, DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B));
+        }
+
+        // Ctor 2
         [Fact]
         public void Test_ctor2_Valid()
         {
@@ -27,11 +47,11 @@ namespace Flapp_TESTS
             Tankkaart tk = new Tankkaart(1, DateTime.Now.AddDays(4));
             Adres a = new Adres("Straat", 1, "Stad", 9000);
             Voertuig v = new Voertuig(1, "MERRY", "THICC", "13245678957903251", "1-ABC-123", new Brandstof("Benzine"), "Auto", "Geel", 4);
-            Bestuurder b = new Bestuurder("Balci", "Burak", "M", a, DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B, v, tk);
+            Bestuurder b = new Bestuurder("Balci", "Burak", Geslacht.M, a, DateTime.Parse("12/05/1999"), "99.05.12-273.26", RijbewijsType.B, v, tk);
 
             Assert.Equal("Balci", b.Naam);
             Assert.Equal("Burak", b.Voornaam);
-            Assert.Equal("M", b.Geslacht);
+            Assert.Equal(Geslacht.M, b.Geslacht);
             Assert.Equal(DateTime.Parse("12/05/1999"), b.Geboortedatum);
             Assert.Equal("99.05.12-273.26", b.Rijksregisternummer);
             Assert.Equal(RijbewijsType.B, b.RijbewijsType);
