aad6752a1d6ffe610b9c1246a024ae52dd80748b
Tibo <Tibo@DESKTOP-8JVOTB1>
2021-10-19 11:15:51 +0200

    Interface

---
+++ Flapp_BLL/Managers/BestuurderManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flapp_BLL.Interfaces;
+using Flapp_BLL.Models;
+
+namespace Flapp_BLL.Managers {
+    public class BestuurderManager {
+        private IBestuurderRepo repo;
+
+        public BestuurderManager(IBestuurderRepo repo) {
+            this.repo = repo;
+        }
+
+        public void VoegBestuurderToe(Bestuurder bestuurder) {
+            try {
+                if (repo.BestaatBestuurder(bestuurder)) { throw new Exception(); }
+                repo.VoegBestuurderToe(bestuurder);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        public void VerwijderBestuurder(Bestuurder bestuurder) {
+            try {
+                if (repo.BestaatBestuurder(bestuurder)) { throw new Exception(); }
+                repo.VerwijderBestuurder(bestuurder);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        public void UpdateBestuurder(Bestuurder bestuurder) {
+            try {
+                if (repo.BestaatBestuurder(bestuurder)) { throw new Exception(); }
+                repo.UpdateBestuurder(bestuurder);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        IReadOnlyList<Bestuurder> GeefAlleTankkaarten() {
+            return;
+        }
+    }
+}
---
+++ Flapp_BLL/Managers/TankkaartManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flapp_BLL.Interfaces;
+using Flapp_BLL.Models;
+
+namespace Flapp_BLL.Managers {
+    public class TankkaartManager {
+        private ITankkaartRepo repo;
+
+        public TankkaartManager(ITankkaartRepo repo) {
+            this.repo = repo;
+        }
+
+        public void VoegTankkaartToe(Tankkaart tankkaart) {
+            try {
+                if(repo.BestaatTankkaart(tankkaart)) { throw new Exception(); }
+                repo.VoegTankkaartToe(tankkaart);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        public void VerwijderTankkaart(Tankkaart tankkaart) {
+            try {
+                if(repo.BestaatTankkaart(tankkaart)) { throw new Exception(); }
+                repo.VerwijderTankkaart(tankkaart);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        public void UpdateTankkaart(Tankkaart tankkaart) {
+            try {
+                if (repo.BestaatTankkaart(tankkaart)) { throw new Exception(); }
+                repo.UpdateTankkaart(tankkaart);
+            }
+            catch (Exception) {
+
+                throw;
+            }
+        }
+        IReadOnlyList<Tankkaart> GeefAlleTankkaarten() {
+            return;
+        }
+    }
+}
