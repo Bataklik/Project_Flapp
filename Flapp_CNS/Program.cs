83866dcdabc8f7cb8e4e1a0f6cb43feb39e5417e
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-21 15:37:13 +0200

    Update

--- Flapp_CNS/Program.cs
+++ Flapp_CNS/Program.cs
@@ -12,32 +12,19 @@ namespace Flapp_CNS
         {
             string connString = @"Data Source=LAPTOP-BURAQ\SQLEXPRESS;Initial Catalog=Project_Flapp_DB;Integrated Security=True";
             //Rijksregisternummer r1 = new Rijksregisternummer("21.10.02-289.65");
-            //RijksregisternummerChecker r2 = new RijksregisternummerChecker("99.05.12-273.26");
+            //Bestuurder b1 = new Bestuurder("Declerck", "Tibo", Geslacht.M, a1, "06/08/1999", "99.08.06-289.17", RijbewijsType.B, v1, t1);
+            Bestuurder b2 = new Bestuurder("Balci", "Burak", Geslacht.M, "12/05/1999", "99.05.12-273.26", RijbewijsType.B);
 
-            Adres a1 = new Adres(1, "Frans Uyttenhovestraat", 91, "Gent", 9000);
+            //Adres a1 = new Adres(1, "Frans Uyttenhovestraat", 91, "Gent", 9000);
 
-            Brandstof bs1 = new Brandstof("Elektrisch");
+            //Brandstof bs1 = new Brandstof("Elektrisch");
 
-            Voertuig v1 = new Voertuig(1, "Tesla", "X", "13245678957903251", "2-ABC-123", bs1, "Stationwagen", "Zwart", 5);
+            //Voertuig v1 = new Voertuig(1, "Tesla", "X", "13245678957903251", "2-ABC-123", bs1, "Stationwagen", "Zwart", 5);
 
-            Tankkaart t1 = new Tankkaart(1, DateTime.Parse("06/08/2025"));
+            //Tankkaart t1 = new Tankkaart(1, DateTime.Parse("06/08/2025"));
 
-            Bestuurder b1 = new Bestuurder("Declerck", "Tibo", Geslacht.M, a1, "06/08/1999", "99.08.06-289.17", RijbewijsType.B, v1, t1);
             BestuurderManager bm = new BestuurderManager(new BestuurderRepo(connString));
-            Console.WriteLine(b1.Rijksregisternummer.Length);
-            bm.VoegBestuurderToe(b1);
-            //Bestuurder b2 = new Bestuurder("Balci", "Burak", DateTime.Parse("12/05/1999"), r2, RijbewijsType.B);
-
-            //Console.WriteLine(r1.ToonNummer());
-            //Console.WriteLine(r2.ToonNummer());
-            //Console.WriteLine(b1);
-            //Console.WriteLine(b2);
-
-            //Console.WriteLine(b2.Naam);
-            //b2.ZetTankkaart(new(1, DateTime.Now.AddDays(1)));
-            //Console.WriteLine(b2.Tankkaart);
-            //b2.ZetNaam("Buraq");
-            //Console.WriteLine(b2.Naam);
+            bm.VoegBestuurderToe(b2);
         }
     }
 }
