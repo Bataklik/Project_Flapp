b6302faf707ec468214b0c6c706b0c6008d68580
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-25 19:43:21 +0200

    Add GeefBrandstof, VoegBrandstofToe, UpdateBrandstof, VerwijderBrandstof

--- Flapp_BLL/Managers/BrandstofManager.cs
+++ Flapp_BLL/Managers/BrandstofManager.cs
@@ -7,20 +7,58 @@ using Flapp_BLL.Interfaces;
 using Flapp_BLL.Models;
 using Flapp_BLL.Exceptions.ManagerExceptions;
 
-namespace Flapp_BLL.Managers {
-    public class BrandstofManager {
+namespace Flapp_BLL.Managers
+{
+    public class BrandstofManager
+    {
         private IBrandstofRepo _repo;
 
-        public BrandstofManager(IBrandstofRepo _repo) {
+        public BrandstofManager(IBrandstofRepo _repo)
+        {
             this._repo = _repo;
         }
 
-        public void GeefBrandstof(Brandstof brandstof) {
-            try {
-                if (!_repo.BestaatBrandstof(brandstof)) { throw new BrandstofManagerException("BrandstofManager: BrandstofType bestaat niet"); }
-                _repo.GeefBrandstof(brandstof);
+        public void GeefBrandstof(Brandstof brandstof)
+        {
+            if (!_repo.BestaatBrandstof(brandstof)) { throw new BrandstofManagerException("BrandstofManager: GeefBrandstof: BrandstofType bestaat niet!"); }
+
+            try { _repo.GeefBrandstof(brandstof); }
+            catch (Exception ex) { throw new BrandstofManagerException("BrandstofManager", ex); }
+        }
+        public void GeefBrandstof(int id)
+        {
+            if (!_repo.BestaatBrandstof(id)) { throw new BrandstofManagerException("BrandstofManager: GeefBrandstof: BrandstofType bestaat niet!"); }
+
+            try { _repo.GeefBrandstof(id); }
+            catch (Exception ex) { throw new BrandstofManagerException("BrandstofManager", ex); }
+        }
+        public void VoegBrandstofToe(Brandstof brandstof)
+        {
+            if (_repo.BestaatBrandstof(brandstof)) { throw new BrandstofManagerException("BrandstofManager: VoegBrandstofToe: BrandstofType bestaat al!"); }
+            try
+            {
+                _repo.VoegBrandstofToe(brandstof);
             }
             catch (Exception ex) { throw new BrandstofManagerException("BrandstofManager", ex); }
+        }
+        public void UpdateBrandstof(Brandstof brandstof)
+        {
+            if (_repo.BestaatBrandstof(brandstof)) { throw new BrandstofManagerException("BrandstofManager: UpdateBrandstof: BrandstofType bestaat al!"); }
+            try
+            {
+                _repo.UpdateBrandstof(brandstof);
+            }
+            catch (Exception ex) { throw new BrandstofManagerException("BrandstofManager", ex); }
+        }
+        public void VerwijderBrandstof(int id)
+        {
+            if (!_repo.BestaatBrandstof(id)) { throw new BrandstofManagerException("BrandstofManager: VerwijderBrandstof: BrandstofType bestaat niet!"); }
+            _repo.VerwijderBrandstof(id);
+        }
+        public void VerwijderBrandstof(string naam)
+        {
+            if (!_repo.BestaatBrandstof(naam)) { throw new BrandstofManagerException("BrandstofManager: VerwijderBrandstof: BrandstofType bestaat niet!"); }
+            _repo.VerwijderBrandstof(naam);
         }
     }
 }
