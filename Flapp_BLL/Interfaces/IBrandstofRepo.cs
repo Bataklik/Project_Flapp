606c5c6c5960d42e55c7f5b5e84a820cea9531f2
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-25 19:41:46 +0200

    Add BestaatBrandstof, VoegBrandstofToe, UpdateBrandstof, VerwijderBrandstof

--- Flapp_BLL/Interfaces/IBrandstofRepo.cs
+++ Flapp_BLL/Interfaces/IBrandstofRepo.cs
@@ -1,16 +1,17 @@
 ﻿using Flapp_BLL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flapp_BLL.Interfaces
 {
     public interface IBrandstofRepo
     {
         Brandstof GeefBrandstof(Brandstof brandstof);
-
+        Brandstof GeefBrandstof(int id);
         bool BestaatBrandstof(Brandstof brandstof);
+        bool BestaatBrandstof(int id);
+        bool BestaatBrandstof(string brandstof_naam);
+        void VoegBrandstofToe(Brandstof brandstof);
+        void UpdateBrandstof(Brandstof brandstof);
+        void VerwijderBrandstof(int id);
+        void VerwijderBrandstof(string brandstof_naam);
     }
 }
