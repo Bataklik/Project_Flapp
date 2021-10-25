8e6966d13f3d0638bc4f1b4de281ac2977e4fcf3
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-10-25 19:39:53 +0200

    Add BestaatBrandstof, UpdateBrandstof, VoegBrandstofToe, VerwijderBrandstof

--- Flapp_DAL/Repository/BrandstofRepo.cs
+++ Flapp_DAL/Repository/BrandstofRepo.cs
@@ -10,27 +10,29 @@ using System.Threading.Tasks;
 
 namespace Flapp_DAL.Repository
 {
-    public class BrandstofRepo : IBrandstofRepo {
+    public class BrandstofRepo : IBrandstofRepo
+    {
         private string _connString;
 
-        public BrandstofRepo(string connString) {
+        public BrandstofRepo(string connString)
+        {
             _connString = connString;
         }
 
-        public bool BestaatBrandstof(Brandstof b) {
+        public bool BestaatBrandstof(Brandstof b)
+        {
             SqlConnection conn = new SqlConnection(_connString);
-            string query = "USE [Project_Flapp_DB]; SELECT 1 FROM Brandstof WHERE naam = @naam;";
-            using (SqlCommand cmd = conn.CreateCommand()) {
+            string query = "USE [Project_Flapp_DB]; SELECT 1 FROM Brandstof WHERE brandstof_naam = @naam;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
                 conn.Open();
-                try {
+                try
+                {
                     cmd.Parameters.Add(new SqlParameter("@naam", SqlDbType.VarChar));
-
                     cmd.CommandText = query;
-
                     cmd.Parameters["@naam"].Value = b.Naam;
 
                     int brandstofBestaat = Convert.ToInt32(cmd.ExecuteScalar());
-
                     if (brandstofBestaat == 1) { return true; }
                     return false;
                 }
@@ -38,22 +40,169 @@ namespace Flapp_DAL.Repository
                 finally { conn.Close(); }
             }
         }
-        public Brandstof GeefBrandstof(Brandstof b) {
+        public bool BestaatBrandstof(int id)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; SELECT 1 FROM Brandstof WHERE id = @id;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@id"].Value = id;
+
+                    int brandstofBestaat = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (brandstofBestaat == 1) { return true; }
+                    return false;
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+
+        public Brandstof GeefBrandstof(Brandstof b)
+        {
             SqlConnection conn = new SqlConnection(_connString);
             string query = "USE [Project_Flapp_DB]; SELECT * FROM Brandstof WHERE naam = @naam;";
-            using (SqlCommand cmd = conn.CreateCommand()) {
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
                 cmd.Parameters.Add(new SqlParameter("@naam", SqlDbType.VarChar));
-
                 cmd.CommandText = query;
-
                 cmd.Parameters["@naam"].Value = b.Naam;
 
                 conn.Open();
-                try {
+                try
+                {
                     SqlDataReader r = cmd.ExecuteReader();
                     r.Read();
-                    Brandstof brandstof = new((string)r["naam"]);
+                    Brandstof brandstof = new((int)r["id"], (string)r["naam"]);
                     return brandstof;
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+        public Brandstof GeefBrandstof(int id)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; SELECT * FROM Brandstof WHERE id = @id;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+                cmd.CommandText = query;
+                cmd.Parameters["@id"].Value = id;
+
+                conn.Open();
+                try
+                {
+                    SqlDataReader r = cmd.ExecuteReader();
+                    r.Read();
+                    Brandstof brandstof = new((int)r["id"], (string)r["naam"]);
+                    return brandstof;
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+
+        public void VoegBrandstofToe(Brandstof b)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; INSERT INTO [dbo].[Brandstof] ([brandstof_naam]) VALUES (@brandstof_naam);";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@brandstof_naam", SqlDbType.VarChar));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@brandstof_naam"].Value = b.Naam;
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+        public void UpdateBrandstof(Brandstof b)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; UPDATE [dbo].[Brandstof] SET [brandstof_naam] = @brandstof_naam WHERE id = @brandstof_id;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@brandstof_id", SqlDbType.Int));
+                    cmd.Parameters.Add(new SqlParameter("@brandstof_naam", SqlDbType.VarChar));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@brandstof_id"].Value = b.Id;
+                    cmd.Parameters["@brandstof_naam"].Value = b.Naam;
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+
+        public void VerwijderBrandstof(int id)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; DELETE FROM [dbo].[Brandstof] WHERE id = @id;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@id"].Value = id;
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+        public void VerwijderBrandstof(string brandstof_naam)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; DELETE FROM [dbo].[Brandstof] WHERE brandstof_naam = @brandstof_naam;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@brandstof_naam", SqlDbType.VarChar));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@brandstof_naam"].Value = brandstof_naam;
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+                finally { conn.Close(); }
+            }
+        }
+
+        public bool BestaatBrandstof(string brandstof_naam)
+        {
+            SqlConnection conn = new SqlConnection(_connString);
+            string query = "USE [Project_Flapp_DB]; SELECT 1 FROM Brandstof WHERE brandstof_naam = @brandstof_naam;";
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter("@brandstof_naam", SqlDbType.VarChar));
+                    cmd.CommandText = query;
+                    cmd.Parameters["@brandstof_naam"].Value = brandstof_naam;
+
+                    int brandstofBestaat = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (brandstofBestaat == 1) { return true; }
+                    return false;
                 }
                 catch (Exception ex) { throw new Exception(ex.Message); }
                 finally { conn.Close(); }
