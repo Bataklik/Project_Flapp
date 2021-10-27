5498c4a6ca6c73340a202bb9cf9110d3fc4a95c7
Tibo <Tibo@DESKTOP-8JVOTB1>
2021-10-27 16:41:39 +0200

    BrandstofUnitTest Test

---
+++ Flapp_Tests/ManagerTesting/BrandstofManager_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Flapp_BLL.Interfaces;
+using Flapp_BLL.Managers;
+using Flapp_BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Flapp_TESTS.ManagerTesting {
+    public class BrandstofManager_UnitTest {
+        private IBrandstofRepo repo;
+        //[Fact]
+        public void VoegBrandstofToe() {
+            Brandstof brandstof = new Brandstof("Benzine");
+            BrandstofManager bm = new BrandstofManager(repo);
+            bm.VoegBrandstofToe(brandstof);
+        }
+    }
+}
