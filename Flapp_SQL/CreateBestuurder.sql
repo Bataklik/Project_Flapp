21f1ee51ea2c446db2d062d702339d655d7a9e7f
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-11-03 10:28:47 +0100

    Update

--- Flapp_SQL/CreateBestuurder.sql
+++ Flapp_SQL/CreateBestuurder.sql
@@ -5,7 +5,6 @@ CREATE TABLE[dbo].[Bestuurder](
    [voornaam] [varchar](50) NOT NULL,
    [geboortedatum] [date] NOT NULL,
    [rijksregister] [varchar](15) NOT NULL,
-   [rijbewijstype_id] [int] FOREIGN KEY REFERENCES dbo.Rijbewijs(id),
    [adres_id] [int] FOREIGN KEY REFERENCES dbo.Adres(id),
    [voertuig_id] [int] FOREIGN KEY REFERENCES dbo.Voertuig(id),
    [tankkaart_id] [int] FOREIGN KEY REFERENCES dbo.Tankkaart(id),
