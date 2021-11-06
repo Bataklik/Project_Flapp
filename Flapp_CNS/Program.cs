a322fbff931cf5f843f107b426321d3f99cb184c
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-11-06 08:26:27 +0100

    Update

--- Flapp_CNS/Program.cs
+++ Flapp_CNS/Program.cs
@@ -25,14 +25,29 @@ namespace Flapp_CNS
 
             Brandstof bs1 = new Brandstof("Elektrisch");
 
-            Voertuig v1 = new Voertuig(1, "Tesla", "X", "13245678957903251", "2-ABC-123", bs1, "Stationwagen", "Zwart", 5);
+            Voertuig Auto1 = new Voertuig(1, "Tesla", "X", "13245678957903251", "2-ABC-123", bs1, "Stationwagen", "Zwart", 5);
+            Voertuig Auto2 = new Voertuig(1, "Golf", "Supreme", "13245678957903251", "2-ABC-113", bs1, "Stationwagen", "Zwart", 5);
 
             Tankkaart t1 = new Tankkaart(1, DateTime.Parse("06/08/2025"));
             List<RijbewijsType> rt = new List<RijbewijsType>();
             rt.Add(new RijbewijsType("B"));
-            Bestuurder b1 = new Bestuurder("Declerck", "Tibo", Geslacht.M, a1, "06/08/1999", "99.08.06-289.17", rt, v1, t1);
+            rt.Add(new RijbewijsType("A"));
+            //(string naam, string voornaam, Geslacht geslacht, string geboortedatum, string rijksregisternummer, List<RijbewijsType> rijbewijs)
+            Bestuurder b1 = new Bestuurder("Declerck", "Tibo", Geslacht.M, "06/08/1999", "99.08.06-289.17", rt);
+            Bestuurder b2 = new Bestuurder("Kcrekced", "Obit", Geslacht.M, "06/08/1999", "99.08.06-289.17", rt);
 
-            Console.WriteLine(rbtm.GeefRijbewijs(1));
+            b1.ZetVoertuig(Auto1);
+
+            Console.WriteLine(b1.Voertuig.Bestuurder);
+            Console.WriteLine(Auto1.Bestuurder);
+
+            Auto1.zetBestuurder(b2);
+            Console.WriteLine(Auto1.Bestuurder);
+            Console.WriteLine(b2.Voertuig.Bestuurder);
+
+            //b2.ZetVoertuig(Auto1);
+            //Console.WriteLine(b2.Voertuig.Bestuurder);
+            //Console.WriteLine(Auto1.Bestuurder);
         }
     }
 }
