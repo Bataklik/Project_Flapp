04bc230b82ecce92946accd0567f8ea3c2154eed
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-11-06 10:17:43 +0100

    Add Toevoegen method in Program

--- Flapp_CNS/Program.cs
+++ Flapp_CNS/Program.cs
@@ -9,45 +9,36 @@ namespace Flapp_CNS
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // Connectie string
-            string connStringBurak = @"Data Source=LAPTOP-BURAQ\SQLEXPRESS;Initial Catalog=Project_Flapp_DB;Integrated Security=True";
+            string connStringBurak = @"Data Source=LAPTOP-BURAQ\SQLEXPRESS;Initial Catalog=Project_Flapp_DB;Integrated Security=True"; ;
 
-            // Managers
-            BrandstofManager bm = new BrandstofManager(new BrandstofRepo(connStringBurak));
-            RijbewijsTypeManager rbtm = new RijbewijsTypeManager(new RijbewijsTypeRepo(connStringBurak));
+            //AdresToevoegen(connStringBurak);
+            //BrandstofToevoegen(connStringBurak);
+            //RijbewijsToevoegen(connStringBurak);
+        }
 
-            // Models
-            Adres a1 = new Adres(1, "Frans Uyttenhovestraat", "91", "Gent", 9000);
+        private static void RijbewijsToevoegen(string connStringBurak)
+        {
+            RijbewijsManager rijbewijsManager = new RijbewijsManager(new RijbewijsRepo(connStringBurak));
+            Rijbewijs rijbewijs1 = new Rijbewijs("AM");
+            rijbewijsManager.VoegRijbewijsToe(rijbewijs1);
+        }
+        private static void AdresToevoegen(string conn)
+        {
+            AdresManager adresManager = new AdresManager(new AdresRepo(conn));
+            Adres adres1 = new Adres("Pannekoekstraat", "7", "Pannekoek", 1111);
 
+            adresManager.VoegAdresToe(adres1);
 
+        }
+        private static void BrandstofToevoegen(string conn)
+        {
+            BrandstofManager brandstofManager = new BrandstofManager(new BrandstofRepo(conn));
+            Brandstof brandstof1 = new Brandstof("Gas");
 
-            Brandstof bs1 = new Brandstof("Elektrisch");
-
-            Voertuig Auto1 = new Voertuig(1, "Tesla", "X", "13245678957903251", "2-ABC-123", bs1, "Stationwagen", "Zwart", 5);
-            Voertuig Auto2 = new Voertuig(1, "Golf", "Supreme", "13245678957903251", "2-ABC-113", bs1, "Stationwagen", "Zwart", 5);
-
-            Tankkaart t1 = new Tankkaart(1, DateTime.Parse("06/08/2025"));
-            List<RijbewijsType> rt = new List<RijbewijsType>();
-            rt.Add(new RijbewijsType("B"));
-            rt.Add(new RijbewijsType("A"));
-            //(string naam, string voornaam, Geslacht geslacht, string geboortedatum, string rijksregisternummer, List<RijbewijsType> rijbewijs)
-            Bestuurder b1 = new Bestuurder("Declerck", "Tibo", Geslacht.M, "06/08/1999", "99.08.06-289.17", rt);
-            Bestuurder b2 = new Bestuurder("Kcrekced", "Obit", Geslacht.M, "06/08/1999", "99.08.06-289.17", rt);
-
-            b1.ZetVoertuig(Auto1);
-
-            Console.WriteLine(b1.Voertuig.Bestuurder);
-            Console.WriteLine(Auto1.Bestuurder);
-
-            Auto1.zetBestuurder(b2);
-            Console.WriteLine(Auto1.Bestuurder);
-            Console.WriteLine(b2.Voertuig.Bestuurder);
-
-            //b2.ZetVoertuig(Auto1);
-            //Console.WriteLine(b2.Voertuig.Bestuurder);
-            //Console.WriteLine(Auto1.Bestuurder);
+            brandstofManager.VoegBrandstofToe(brandstof1);
         }
     }
 }
