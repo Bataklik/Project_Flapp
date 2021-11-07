07e1206ccc641211ce04d99adbd96b5b130b514e
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-11-07 11:51:44 +0100

    Update WPF

---
+++ Flapp_PL/View/UserControls/TankkaartUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Flapp_PL.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TankkaartUC.xaml
+    /// </summary>
+    public partial class TankkaartUC : UserControl
+    {
+        public TankkaartUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
