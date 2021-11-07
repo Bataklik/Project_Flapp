de3927dfab5e1905752534ca8f8ea560e242a6cb
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-11-07 16:39:21 +0100

    Update SQL File

--- Flapp_SQL/InsertData.sql
+++ Flapp_SQL/InsertData.sql
@@ -4,7 +4,7 @@ USE [Project_Flapp_DB]; INSERT INTO [dbo].[Adres] ([straat] ,[huisnummer] ,[stad
 
 -- Bestuurder Insert
 USE [Project_Flapp_DB]; INSERT INTO [dbo].[Bestuurder] ([naam] ,[voornaam] ,[geboortedatum] ,[rijksregister] ,[adresId] ,[voertuigId] ,[tankkaartId] ,[geslacht]) VALUES ('Declerck' ,'Tibo' ,'06/08/1999' ,'99.08.06-289.17' ,NULL ,NULL ,NULL ,1);
-USE [Project_Flapp_DB]; INSERT INTO [dbo].[Bestuurder] ([naam] ,[voornaam] ,[geboortedatum] ,[rijksregister] ,[adresId] ,[voertuigId] ,[tankkaartId] ,[geslacht]) VALUES ('Balci' ,'Burak' ,'12/05/1999' ,'99.05.12-289.17' ,2 ,NULL ,NULL ,1);
+USE [Project_Flapp_DB]; INSERT INTO [dbo].[Bestuurder] ([naam] ,[voornaam] ,[geboortedatum] ,[rijksregister] ,[adresId] ,[voertuigId] ,[tankkaartId] ,[geslacht]) VALUES ('Balci' ,'Burak' ,'12/05/1999' ,'99.05.12-289.17' ,2 ,1 ,1 ,1);
 
 -- Brandstof Insert
 USE [Project_Flapp_DB]; INSERT INTO [dbo].[Brandstof] ([naam]) VALUES ('Benzine'); 
