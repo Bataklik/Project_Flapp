70b99ef2bbb7abeb777c961ae89d0498106d6ed9
CoopahG <tibo.declerck@student.hogent.be>
2021-11-26 17:01:55 +0100

    update tankkaartwpf

--- Flapp_PL/View/Windows/TankkaartWindows/TankkaartZoekBestuurderWindow.xaml.cs
+++ Flapp_PL/View/Windows/TankkaartWindows/TankkaartZoekBestuurderWindow.xaml.cs
@@ -34,5 +34,13 @@ namespace Flapp_PL.View.Windows.TankkaartWindows {
             }
             catch (Exception) { throw new Exception(); }
         }
+                
+        private void btnBestuurder_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnAnnuleer_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
     }
 }
