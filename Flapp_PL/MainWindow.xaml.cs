b74bdf3e58917a6cb4ebeaee1d35766bc7543387
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-12-25 19:26:56 +0100

    Add Azure connection

--- Flapp_PL/MainWindow.xaml.cs
+++ Flapp_PL/MainWindow.xaml.cs
@@ -1,61 +1,60 @@
 ﻿using Flapp_BLL.Managers;
 using Flapp_DAL.Repository;
 using Flapp_PL.View.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Windows;
-namespace Flapp_PL
-{
-    public partial class MainWindow : Window
-    {
+namespace Flapp_PL {
+    public partial class MainWindow : Window {
         ConnectionManager cm;
-        public MainWindow()
-        {
+        public MainWindow() {
             cm = new ConnectionManager(new ConnectionRepo((string)Application.Current.Properties["User"]));
             InitializeComponent();
-            cbUsers.ItemsSource = new List<string> { "Raf", "TiboD", "TiboL", "Burak" };
-            if (!cm.IsServerConnected())
-            {
-                btnBestuurder.IsEnabled = false;
-                btnVoertuig.IsEnabled = false;
-                btnTankkaart.IsEnabled = false;
-            }
-            else
-            {
-                btnBestuurder.IsEnabled = true;
-                btnVoertuig.IsEnabled = true;
-                btnTankkaart.IsEnabled = true;
-            }
+            cbUsers.ItemsSource = new List<string> { "Raf", "TiboD", "TiboL", "Burak", "Azure" };
+            toggleButtons(cm.IsServerConnected());
+            //if (!cm.IsServerConnected()) {
+            //    btnBestuurder.IsEnabled = false;
+            //    btnVoertuig.IsEnabled = false;
+            //    btnTankkaart.IsEnabled = false;
+            //}
+            //else {
+            //    btnBestuurder.IsEnabled = true;
+            //    btnVoertuig.IsEnabled = true;
+            //    btnTankkaart.IsEnabled = true;
+            //}
         }
 
-        private void btnBestuurder_Click(object sender, RoutedEventArgs e)
-        {
+        private void btnBestuurder_Click(object sender, RoutedEventArgs e) {
             MainWindow main = this;
             wpUserControl.Children.Clear();
             BestuurderUC bestuurderUc = new BestuurderUC(main);
             wpUserControl.Children.Add(bestuurderUc);
             main.Width = 750;
         }
-        private void btnVoertuig_Click(object sender, RoutedEventArgs e)
-        {
+        private void btnVoertuig_Click(object sender, RoutedEventArgs e) {
             MainWindow main = this;
 
             wpUserControl.Children.Clear();
             VoertuigUC voertuigUc = new VoertuigUC(main);
             wpUserControl.Children.Add(voertuigUc);
         }
-        private void btnTankkaart_Click(object sender, RoutedEventArgs e)
-        {
+        private void btnTankkaart_Click(object sender, RoutedEventArgs e) {
             MainWindow main = this;
             wpUserControl.Children.Clear();
             TankkaartUC tankkaartUc = new TankkaartUC(main);
             wpUserControl.Children.Add(tankkaartUc);
         }
 
-        private void btnKies_Click(object sender, RoutedEventArgs e)
-        {
-            switch (cbUsers.SelectedItem)
-            {
+        private void toggleButtons(bool value) {
+            btnBestuurder.IsEnabled = value;
+            btnVoertuig.IsEnabled = value;
+            btnTankkaart.IsEnabled = value;
+            wpUserControl.Children.Clear();
+        }
+
+        private void btnKies_Click(object sender, RoutedEventArgs e) {
+            switch (cbUsers.SelectedItem) {
                 case "Raf":
                     Application.Current.Properties["User"] = ConfigurationManager.ConnectionStrings["connStringR"].ConnectionString;
                     break;
@@ -68,24 +67,29 @@ namespace Flapp_PL
                 case "Burak":
                     Application.Current.Properties["User"] = ConfigurationManager.ConnectionStrings["connStringB"].ConnectionString;
                     break;
+                case "Azure":
+                    Application.Current.Properties["User"] = ConfigurationManager.ConnectionStrings["connStringA"].ConnectionString;
+                    break;
             }
-            cm = new ConnectionManager(new ConnectionRepo((string)Application.Current.Properties["User"]));
-            if (!cm.IsServerConnected())
-            {
-                btnBestuurder.IsEnabled = false;
-                btnVoertuig.IsEnabled = false;
-                btnTankkaart.IsEnabled = false;
-                wpUserControl.Children.Clear();
-                MessageBox.Show("Connectie niet gelukt!", "Connection Failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+            try {
+                cm = new ConnectionManager(new ConnectionRepo((string)Application.Current.Properties["User"]));
             }
-            else
-            {
-                btnBestuurder.IsEnabled = true;
-                btnVoertuig.IsEnabled = true;
-                btnTankkaart.IsEnabled = true;
-                MessageBox.Show("Connectie gekozen!", "Connection Successful!", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
 
+            toggleButtons(cm.IsServerConnected());
+            //if (!cm.IsServerConnected()) {
+            //    btnBestuurder.IsEnabled = false;
+            //    btnVoertuig.IsEnabled = false;
+            //    btnTankkaart.IsEnabled = false;
+            //    wpUserControl.Children.Clear();
+            //    MessageBox.Show("Connectie niet gelukt!", "Connection Failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+            //else {
+            //    btnBestuurder.IsEnabled = true;
+            //    btnVoertuig.IsEnabled = true;
+            //    btnTankkaart.IsEnabled = true;
+            //    MessageBox.Show("Connectie gekozen!", "Connection Successful!", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
         }
     }
 }
