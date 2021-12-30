472e4c38ac6bd61da472d01abfb6d4813d907215
Bataklik <60544395+bataklik1213@users.noreply.github.com>
2021-12-30 22:43:24 +0100

    Update WPF

--- Flapp_PL/View/Windows/BeheerWindows/Adresbeheer.xaml.cs
+++ Flapp_PL/View/Windows/BeheerWindows/Adresbeheer.xaml.cs
@@ -6,16 +6,13 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 
-namespace Flapp_PL.View.Windows.BestuurderWindows.BeheerWindows
-{
-    public partial class Adresbeheer : Window
-    {
+namespace Flapp_PL.View.Windows.BestuurderWindows.BeheerWindows {
+    public partial class Adresbeheer : Window {
         private VoegBestuurderToe _parentWindow;
         private AdresManager _adresManager;
         private UpdateBestuurderWindow _parentUpdateWindow;
 
-        public Adresbeheer(VoegBestuurderToe parentWindow)
-        {
+        public Adresbeheer(VoegBestuurderToe parentWindow) {
             InitializeComponent();
 
             _parentWindow = parentWindow;
@@ -23,8 +20,7 @@ namespace Flapp_PL.View.Windows.BestuurderWindows.BeheerWindows
 
             LaadAdressen();
         }
-        public Adresbeheer(UpdateBestuurderWindow parentWindow)
-        {
+        public Adresbeheer(UpdateBestuurderWindow parentWindow) {
             InitializeComponent();
 
             _parentUpdateWindow = parentWindow;
@@ -33,56 +29,47 @@ namespace Flapp_PL.View.Windows.BestuurderWindows.BeheerWindows
             LaadAdressen();
         }
 
-        public void LaadAdressen()
-        {
-            try
-            {
-                lstAdressen.ItemsSource = _adresManager.GeefAdressen();
-                cbSteden.ItemsSource = _adresManager.GeefAlleSteden();
-            }
-            catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
-        }
-        private void miSelecteer_Click(object sender, RoutedEventArgs e)
-        {
+
+        #region Click Methods
+        private void miSelecteer_Click(object sender, RoutedEventArgs e) {
             if (lstAdressen.SelectedItems == null) { MessageBox.Show("U heeft geen adres geselecteerd!"); return; }
-            if (_parentWindow == null)
-            {
+            if (_parentWindow == null) {
                 _parentUpdateWindow.lstAdres.Items.Clear();
                 _parentUpdateWindow.lstAdres.Items.Add((Adres)lstAdressen.SelectedItem);
             }
-            else
-            {
+            else {
                 _parentWindow.lstAdres.Items.Clear();
                 _parentWindow.lstAdres.Items.Add((Adres)lstAdressen.SelectedItem);
             }
             Close();
         }
-        private void miVoegToe_Click(object sender, RoutedEventArgs e)
-        {
+        private void miVoegToe_Click(object sender, RoutedEventArgs e) {
             new VoegAdresToe(this, _adresManager).ShowDialog();
         }
-        private void miVerwijderen_Click(object sender, RoutedEventArgs e)
-        {
+        private void miVerwijderen_Click(object sender, RoutedEventArgs e) {
             if (lstAdressen.SelectedItem == null) { MessageBox.Show("U heeft geen adres geselecteerd"); }
-            try
-            {
+            try {
                 _adresManager.VerwijderAdres((Adres)lstAdressen.SelectedItem);
                 LaadAdressen();
             }
             catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
         }
-        private void cbSteden_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            if (cbSteden.SelectedItem == null) { MessageBox.Show("Geen Juiste stad aangeduid!"); return; }
-            KeyValuePair<int, string> stad = (KeyValuePair<int, string>)cbSteden.SelectedItem;
-            try { cbStraten.ItemsSource = _adresManager.GeefStratenStad(stad.Key, stad.Value); }
-            catch (Exception) { throw; }
+        private void btnZoek_Click(object sender, RoutedEventArgs e) {
+            lstAdressen.ItemsSource = _adresManager.ZoekAdressen(toUpperFirstletter(txtSteden.Text), toUpperFirstletter(txtStraten.Text));
         }
-        private void btnZoek_Click(object sender, RoutedEventArgs e)
-        {
-            if (cbSteden.SelectedItem == null || cbStraten.SelectedItem == null) { MessageBox.Show("Geen Juiste stad of straat aangeduid!"); return; }
-            KeyValuePair<int, string> stad = (KeyValuePair<int, string>)cbSteden.SelectedItem;
-            lstAdressen.ItemsSource = _adresManager.ZoekAdressen(stad.Key, stad.Value, (string)cbStraten.SelectedItem);
+        #endregion
+
+        #region Helpers
+        private string toUpperFirstletter(string value) {
+            if (string.IsNullOrWhiteSpace(value)) { return ""; }
+            return char.ToUpper(value[0]) + value.Substring(1).ToLower();
         }
+        public void LaadAdressen() {
+            try {
+                lstAdressen.ItemsSource = _adresManager.GeefAdressen();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+        }
+        #endregion
     }
 }
